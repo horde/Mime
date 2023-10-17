fe961f4401dfccaba1bc9bd8d40fcf5e4ecc7999
Michael J Rubinsky <mrubinsk@horde.org>
2023-10-16 22:50:59 -0400

    Attempt to sniff out if Thread-Index hdr needs mime encoding

--- lib/Horde/Mime/Headers/ThreadIndex.php
+++ lib/Horde/Mime/Headers/ThreadIndex.php
@@ -16,6 +16,56 @@ class Horde_Mime_Headers_ThreadIndex extends Horde_Mime_Headers_Element_Single
      */
     protected function _sendEncode($opts)
     {
-        return array(Horde_Mime::encode($this->value, $opts['charset']));
+        $encoded = Horde_Mime::encode($this->value, $opts['charset']);
+
+        if ($encoded != $this->value) {
+            return array($this->value);
+        }
+
+        $len = strlen($this->value);
+        $crlf = 0;
+        $needs_encoding = false;
+        for ($i = 0; $i < $len; ++$i) {
+            $chr = ord($this->value[$i]);
+            switch ($chr) {
+            case 0:
+                // NULLs not valid here, should have
+                // been caught above.
+                return array('');
+            case 10:
+            case 13:
+                $crlf = 0;
+                break;
+            default:
+                if (++$crlf > 998) {
+                    $needs_encoding = true;
+                    break 2;
+                }
+                break;
+            }
+        }
+
+        if ($needs_encoding) {
+            $delim = '=?' . $opts['charset'] . '?b?';
+            $parts = explode(
+                self::EOL,
+                rtrim(
+                    chunk_split(
+                        base64_encode($this->value),
+                        /* strlen($delim) + 2 = space taken by MIME
+                         * delimiter */
+                        intval((75 - strlen($delim) + 2) / 4) * 4
+                    )
+                )
+            );
+            $tmp = array();
+            foreach ($parts as $val) {
+                $tmp[] = $delim . $val . '?=';
+            }
+
+            return trim(implode(' ', $tmp));
+        }
+
+        return array($this->value);
     }
 }
