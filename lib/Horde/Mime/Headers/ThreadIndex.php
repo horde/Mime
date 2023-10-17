96afe59088d4d4a3e14a6ee94578d310b2be3edc
Michael J Rubinsky <mrubinsk@horde.org>
2023-10-16 22:06:50 -0400

    First attempt at supporting (broken) MS Thread-Index headers.

---
+++ lib/Horde/Mime/Headers/ThreadIndex.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ */
+class Horde_Mime_Headers_ThreadIndex extends Horde_Mime_Headers_Element_Single
+{
+    public static getHandlers()
+    {
+        return array(
+            'thread-index'
+        );
+    }
+
+    /**
+     * TODO
+     */
+    protected function _sendEncode($opts)
+    {
+
+        return array(Horde_Mime::encode($this->values, $opts['charset']));
+    }
+}
